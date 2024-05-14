9dcad186fd55bebb07d5989e250d23c9d33c0639
Dustrunner369 <matthewswift369@gmail.com>
2024-05-13 18:29:52 -0700

    api endpoints exposed

--- Models/BookModel.cs
+++ Models/BookModel.cs
@@ -2,5 +2,8 @@ namespace WebAppAPI.Models;
 
 public class BookModel
 {
-    
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public double Price { get; set; }
+    public int BookID { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using WebAppAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,29 +18,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+//Basic CRUD operations
+app.MapGet("/", () => "This is a GET");
+app.MapPost("/", () => "This is a POST");
+app.MapPut("/", () => "This is a PUT");
+app.MapDelete("/", () => "This is a DELETE");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
